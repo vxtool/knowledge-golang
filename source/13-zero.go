3402a8e617d3ef2f0072fdb7e40ab5ebcc740c56
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-04 18:11:22 -0300

    add: zero

---
+++ source/13-zero.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+  var i int
+  var f float64
+  var b bool
+  var s string
+  fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
+
+// Valores zero
+// Variáveis declaradas sem um valor inicial explicitado darão seu valor zero.
+// O valor zero é:
+//     0 para tipos numéricos,
+//     false para tipos boleanos, e
+//     "" (string vazia) para strings.
