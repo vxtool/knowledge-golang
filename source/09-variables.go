4f8e61a32cc2d4e3ea938ba100ead0e437cb5831
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-04 18:10:13 -0300

    add: variables

---
+++ source/09-variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+  var i int
+  fmt.Println(i, c, python, java)
+}
+
+// Variáveis
+// A instrução var declara uma lista de variáveis, como em listas de argumentos de função, o tipo é o último passado.
+// A declaração var pode estar num pacote ou a nível de função. Nós vemos ambos neste exemplo.
