26a44638a803bfc777cd910f02a0ec4185ce82c9
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-04 18:12:53 -0300

    add: constants

---
+++ source/16-constants.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+  const World = "Hemerson"
+  fmt.Println("Hello", World)
+  fmt.Println("Happy", Pi, "Day")
+
+  const Truth = true
+  fmt.Println("Go rules?", Truth)
+}
+
+// Constantes
+// Constantes são declaradas como variáveis, mas com a palavra-chave const.
+// Constantes podem ser seqüências de caracteres, booleanos, ou valores numéricos.
+// Constantes não podem ser declaradas usando a sintaxe :=.
