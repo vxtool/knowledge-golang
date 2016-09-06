06f923db778454b66e7eae7c182d46a4b9e243c3
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-05 22:27:45 -0300

    add: forever

---
+++ source/21-forever.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+  for {
+  }
+}
+
+// Para sempre (Forever)
+// Se você omitir a condição do laço, ele executará para sempre, e assim um loop infinito é compactadamente expresso.
