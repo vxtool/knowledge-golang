465ef272a95a226f4d0e6dc056d02cfe179460ad
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-05 22:27:57 -0300

    add: if

---
+++ source/22-if.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+func sqrt(x float64) string {
+  if x < 0 {
+    return sqrt(-x) + "i"
+  }
+  return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+  fmt.Println(sqrt(2), sqrt(-4))
+}
+
+// If
+// A declaração if parece com o que o de C ou Java fazem, exceto que as ( ) se foram (não são nem mesmo opcionais) e os { } são obrigatórios.
+// (Parece familiar?)
