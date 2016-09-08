7cde38add478b408e04b17618b3267f3072670b4
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-07 22:30:26 -0300

    add: defer

---
+++ source/29-defer.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  defer fmt.Println("world")
+
+  fmt.Println("hello")
+}
+
+// Defer
+// A declaração defer adia a execução de uma função até o final do retorno da função.
+// Os argumentos das chamadas adiadas são avaliados imediatamente, mas a função chamada não é executada até o retorno da função.
