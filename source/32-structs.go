29fc2dfad829cfe5e3a84b3f259b92113b39d52b
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-08 21:41:49 -0300

    add: structs

---
+++ source/32-structs.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+  X int
+  Y int
+}
+
+func main() {
+  fmt.Println(Vertex{1, 2})
+}
+
+// Structs
+// A struct é uma coleção de campos.
+// (E uma declaração type faz o que você espera.)
