766991372182df7886621058923da5ef0b874afa
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-08 21:44:10 -0300

    add: range

---
+++ source/42-range.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func main() {
+  for i, v := range pow {
+    fmt.Printf("2**%d = %d\n", i, v)
+  }
+}
+
+// Range
+// O range do laço for itera sobre uma slice ou map.
